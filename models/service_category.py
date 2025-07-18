355752c4586aa40944112c02de11e3e9733d65c3
Migueldevv21 <angels130102@gmail.com>
2025-07-18 09:17:36 -0500

    Agrega modelo ServiceCategory

---
+++ models/service_category.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime
+from datetime import datetime
+from database import Base
+
+class ServiceCategory(Base):
+    __tablename__ = "service_category"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(Text, nullable=True)
+    image_url = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
